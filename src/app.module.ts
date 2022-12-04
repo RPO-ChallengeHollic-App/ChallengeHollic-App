8076d9ed2bd0980101a812ba2333295917db6e19
aljaz-kokol <kokol.aljaz@gmail.com>
2022-12-04 17:57:32 +0100

    Import fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { GroupModule } from './group/group.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, GroupModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UserModule],
   controllers: [],
   providers: [],
 })
